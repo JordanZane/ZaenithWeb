2ba4df54fa15f5ed59f66e743d53d755170a7b69
Zanetti Jordan <jzanetti1@outlook.fr>
2024-02-23 14:01:38 +0100

    Add styles contact section & form

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -16,25 +16,39 @@ const Contact = () => {
             Une idée ? Projeter votre vision <span>dans le web</span>
           </h3>
           <form action="">
-            <label htmlFor="name">Votre nom*</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="email">Votre e-mail*</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="subject">Sujet*</label>
-            <select name="subject" id="subject">
-              <option value="site-vitrine">Site vitrine</option>
-              <option value="site-vitrine">Site e-commerce</option>
-              <option value="site-vitrine">Autre</option>
-            </select>
-            <label htmlFor="company">Nom d'entreprise*</label>
-            <input type="text" id="company" name="company" />
-            <label htmlFor="message">Message*</label>
-            <textarea
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-            ></textarea>
+            <div className="form-content">
+              <label htmlFor="name">Votre nom*</label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className="form-content">
+              <label htmlFor="email">Votre e-mail*</label>
+              <input type="email" id="email" name="email" />
+            </div>
+            <div className="form-content">
+              <label htmlFor="subject">Sujet*</label>
+              <select name="subject" id="subject">
+                <option value=""></option>
+                <option value="site-vitrine">Site vitrine</option>
+                <option value="site-vitrine">Site e-commerce</option>
+                <option value="site-vitrine">Autre</option>
+              </select>
+            </div>
+            <div className="form-content">
+              <label htmlFor="company">Nom d'entreprise*</label>
+              <input type="text" id="company" name="company" />
+            </div>
+            <div className="form-content">
+              <label htmlFor="message">Message*</label>
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+            <div className="btn-container">
+              <div className="btn-style">C'est parti !</div>
+            </div>
           </form>
         </div>
       </div>

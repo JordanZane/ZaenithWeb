ffd3f93d197068e417f1e55b1f5ee5a34703d57b
Zanetti Jordan <jzanetti1@outlook.fr>
2024-03-03 09:13:35 +0100

    Add frontend logic to handle submit form & separate component form contact form

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactForm from '../ContactForm/ContactForm';
 import MediumLogo from '../../assets/images/medium-logo-icon.svg';
 
 const Contact = () => {
@@ -15,42 +16,7 @@ const Contact = () => {
           <h3 className="hidden">
             Une idée ? Projeter votre vision <span>dans le web</span>
           </h3>
-          <form action="">
-            <div className="form-content hidden name-input">
-              <label htmlFor="name">Votre nom*</label>
-              <input type="text" id="name" name="name" />
-            </div>
-            <div className="form-content hidden email-input">
-              <label htmlFor="email">Votre e-mail*</label>
-              <input type="email" id="email" name="email" />
-            </div>
-            <div className="form-content hidden subject-input">
-              <label htmlFor="subject">Sujet*</label>
-              <select name="subject" id="subject">
-                <option value=""></option>
-                <option value="site-vitrine">Site vitrine</option>
-                <option value="site-vitrine">Site e-commerce</option>
-                <option value="site-vitrine">Autre</option>
-              </select>
-            </div>
-            <div className="form-content hidden company-input">
-              <label htmlFor="company">Nom d'entreprise*</label>
-              <input type="text" id="company" name="company" />
-            </div>
-            <div className="form-content hidden message-input">
-              <label htmlFor="message">Message*</label>
-              <textarea
-                name="message"
-                id="message"
-                cols="30"
-                rows="10"
-              ></textarea>
-            </div>
-            <p className="required-field hidden">*Champs requis</p>
-            <div className="btn-container">
-              <div className="btn-style hidden">C'est parti !</div>
-            </div>
-          </form>
+          <ContactForm />
         </div>
       </div>
     </section>

9c0da571edbb4184b7863ef44ea780de896863ef
Zanetti Jordan <jzanetti1@outlook.fr>
2024-03-05 10:47:48 +0100

    Responsive Loader

--- app/src/components/Loader/Loader.jsx
+++ app/src/components/Loader/Loader.jsx
@@ -3,15 +3,16 @@ const ScrollAnim = React.lazy(() => import('../ScrollAnim/ScrollAnim'));
 const Loader = () => {
   return (
     <>
-      {' '}
       <ScrollAnim />
       <div className="loader-container">
+        <div className="blue-blur-circle"></div>
         <div className="triangle-container">
           <svg className="triangle" viewBox="0 0 100 100">
             <path className="hidden" d="M 50 0 L 100 100 L 0 100 Z" />
           </svg>
           <p>Loading...</p>
         </div>
+        <div className="green-blur-circle"></div>
       </div>
     </>
   );
